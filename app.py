7e8bd59c974478a92b337ac3c702f2a082d7d8e3
Suyash Pandey <92623123+SP4567@users.noreply.github.com>
2024-08-14 14:17:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import load_model
 
 # Download necessary NLTK data
 nltk_data_dir = '/home/appuser/nltk_data'
-nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('punkt_tab', download_dir=nltk_data_dir)
 os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.append(nltk_data_dir)
 nltk.download('stopwords', download_dir=nltk_data_dir)
